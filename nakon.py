05d80f0876789ecf1b9d8a16a81db1a67bc22708
0xL30N3 <dudepro1111@gmail.com>
2026-06-10 14:52:23 -0400

    initial vuln loading implementation

--- nakon.py
+++ nakon.py
@@ -22,25 +22,35 @@ mydb = mysql.connector.connect(
   password=os.getenv("password"),
   database=os.getenv("database")
 )
-
 cursor = mydb.cursor()
-cursor.execute("SELECT * FROM misconfigs;")
 
-myresult = cursor.fetchall()
-
-for x in myresult:
-    print(x)
-
-print(config['machines'][1])
-
+#main loop
 for machine in config['machines']:
     client = paramiko.SSHClient()
     #fixes unknown host key stuff
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     #use creds from config
     client.connect(hostname=machine["ip"], username=machine["user"], password=machine["password"])
-    stdin, stdout, stderr = client.exec_command('sudo -S id')
-    sudo_pass()
+
+    #secondary loop for vulns
+    for vuln in machine["vulns"]:
+        cursor.execute("SELECT * FROM misconfigs WHERE id=" + str(vuln["id"]))
+        item = cursor.fetchall()
+
+        #0=id, 1=vuln_id, 2=type, 3=script, 4=runas
+        runAs = item[0][4]
+        cmd = item[0][3]
+        stdin, stdout, stderr = client.exec_command("cat << EOF > /tmp/cmd.sh\n" + cmd + "\nEOF")
+        stdin, stdout, stderr = client.exec_command("chmod +x /tmp/cmd.sh")
+        if runAs == "root":
+            stdin, stdout, stderr = client.exec_command("sudo -S sh /tmp/cmd.sh")
+            sudo_pass()
+            print(stdout.read().decode())
+            print(stderr.read().decode())
+        else:
+            stdin, stdout, stderr = client.exec_command(cmd)
+            print(stdout.read().decode())
+            print(stderr.read().decode())
 
     print(stdout.read().decode())
     client.close()
